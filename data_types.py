cc4109f3122b0f45d6f94697b646921527b31dc5
Laxman <laxmank4496@gmail.com>
2026-08-18 17:40:23 +0530

    Data Types in python

---
+++ data_types.py
@@ -0,0 +1,23 @@
+# Demonstration of different data types in Python
+
+a = 10                  # Integer
+b = 10.5                # Float
+c = 2 + 3j              # Complex
+d = True                # Boolean
+e = "Python"            # String
+f = [1, 2, 3]           # List
+g = (1, 2, 3)           # Tuple
+h = {1, 2, 3}            # Set
+i = {"name": "Rahul"}   # Dictionary
+j = None                # NoneType
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
+print(i, type(i))
+print(j, type(j))
